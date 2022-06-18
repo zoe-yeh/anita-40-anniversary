ae83cfb5b3dfc9306b3b13d66fde746f2003d88e
葉憶雯 <yeah821231@gmail.com>
2022-06-18 23:55:57 +0800

    modification

--- anita-40-anniversary.py
+++ anita-40-anniversary.py
@@ -8,20 +8,27 @@ import streamlit as st
 from google.cloud import firestore, storage
 from google.oauth2 import service_account
 from google.auth.transport import mtls
-from firebase import firebase
 from PIL import Image
 from datetime import datetime
 
 
 db = firestore.Client.from_service_account_json(".firestore-key.json")
 storage_client = storage.Client.from_service_account_json(".gcs-key.json")
-bucket_name = "anita-40-anniversary-test"
+# bucket_name = "anita-40-anniversary-test"
+bucket_name = "anita-40-anniversary-prod"
+
+# confirm local folder exists
+_directory_ = '/home/ubuntu/anita_photo/' + \
+        datetime.today().strftime('%Y_%m_%d')
+bucket = storage_client.get_bucket(bucket_name)
+if not os.path.exists(_directory_):
+    os.makedirs(_directory_)
 
 # 網頁配置設定
 st.set_page_config(
     page_title="Anita Mui 出道 40 週年應援活動",
     page_icon="🎤",
-#    layout="wide",
+   layout="wide",
     initial_sidebar_state="collapsed")
 
 def local_css(file_name):
@@ -50,54 +57,18 @@ def load_image(image_file):
     return img
 
 
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-# set_background('/home/ubuntu/anita_photo/2022_05_22/115d1232-d985-11ec-b255-859fa7354fc2-20211230mui_0.png')
-
-
 # Streamlit widgets to let a user create a new post
 unique_id = str(uuid.uuid1())
 today = st.date_input("今天日期")
 nickname = st.text_input("暱稱:")
-
 image_file = st.file_uploader(
-    "我們先來為今天留下美好的紀念吧！放張你最喜歡的 Anita 或是為今天留下一個紀念吧！ P.S.可以上傳多張照片唷！", accept_multiple_files=True)
-
+    "先讓我們為今天留下美好的紀念吧！放張你最喜歡的 Anita 或是為今天留下一個紀念吧！ P.S.可以上傳多張照片唷！", accept_multiple_files=True)
 place = st.text_input("hello! 請問你是來自哪裡的粉絲:")
 years = st.text_input(f"想請問一下，你喜歡 Anita 已經有幾年了？")
 sentence = st.text_area(f"那麽你最想對 Anita 說什麼呢？", height=100)
-submit = st.button("我答完了")
 
-# Once the user has submitted, upload it to the Firebase database
-if nickname and place and years and sentence and submit:
-    doc_ref = db.collection("anita").document(unique_id)
-    doc_ref.set({
-        "uuid": unique_id,
-        "date_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "nickname": nickname,
-        "place": place,
-        "year": years,
-        "sentence": sentence
-    })
-    st.subheader(
-        f"我們收到囉! 我們會將你們想對 Anita 說的話整理起來，一切消息都會公佈在小海 Instagram 帳號! 敬請期待 😆")
-
+st.markdown("<h4 style='text-align: center; color: black;'>讓我們來看看今天留下的回憶吧！</h4>",
+            unsafe_allow_html=True)
 # display uploaded images
 if image_file is not None:
     # To View Uploaded Image
@@ -135,18 +106,70 @@ if image_file is not None:
                 st.image(img_list[0], width=300, use_column_width='Ture')
             with col3:
                 st.write(' ')
+# # Once the user has submitted, upload it to the Firebase database
+# if nickname and place and years and sentence and image_file is not None:
+#     doc_ref = db.collection("anita-40-anniversary").document(unique_id)
+#     doc_ref.set({
+#         "uuid": unique_id,
+#         "date_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+#         "nickname": nickname,
+#         "place": place,
+#         "year": years,
+#         "sentence": sentence
+#     })
+
+#     for image in image_file:
+#         local_fileName = unique_id + '-' + image.name
+#         gcs_fileName = unique_id + '/' + image.name
+
+#         with open(os.path.join(_directory_, local_fileName), "wb") as f:
+#             f.write(image.getbuffer())
+
+#         # upload image to GCS
+#         blob = bucket.blob(gcs_fileName)
+#         blob.upload_from_filename(os.path.join(_directory_, local_fileName))
+    
+
+# # Once the user has submitted, upload it to the Firebase database
+# if nickname and place and years and sentence:
+#     doc_ref = db.collection("anita").document(unique_id)
+#     doc_ref.set({
+#         "uuid": unique_id,
+#         "date_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+#         "nickname": nickname,
+#         "place": place,
+#         "year": years,
+#         "sentence": sentence
+#     })
+#     st.subheader(
+#         f"我們收到囉! 我們會將你們想對 Anita 說的話整理起來，一切消息都會公佈在小海 Instagram 帳號! 敬請期待 😆")
+
+# if image_file is not None:
+#     # save image to local
+#     for image in image_file:
+#         local_fileName = unique_id + '-' + image.name
+#         gcs_fileName = unique_id + '/' + image.name
+
+#         with open(os.path.join(_directory_, local_fileName), "wb") as f:
+#             f.write(image.getbuffer())
+
+#         # upload image to GCS
+#         blob = bucket.blob(gcs_fileName)
+#         blob.upload_from_filename(os.path.join(_directory_, local_fileName))
 
 
-if image_file is not None:
-    _directory_ = '/home/ubuntu/anita_photo/' + \
-        datetime.today().strftime('%Y_%m_%d')
-    bucket = storage_client.get_bucket(bucket_name)
+# Once the user has submitted, upload it to the Firebase database
+if nickname and place and years and sentence and image_file is not None:
+    doc_ref = db.collection("anita-40-anniversary").document(unique_id)
+    doc_ref.set({
+        "uuid": unique_id,
+        "date_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "nickname": nickname,
+        "place": place,
+        "year": years,
+        "sentence": sentence
+    })
 
-    # confirm local folder exists
-    if not os.path.exists(_directory_):
-        os.makedirs(_directory_)
-
-    # save image to local
     for image in image_file:
         local_fileName = unique_id + '-' + image.name
         gcs_fileName = unique_id + '/' + image.name
@@ -157,6 +180,9 @@ if image_file is not None:
         # upload image to GCS
         blob = bucket.blob(gcs_fileName)
         blob.upload_from_filename(os.path.join(_directory_, local_fileName))
+    
+st.subheader(
+        f"你們想對 Anita 說的話我們已經收到囉! 我們會將這些寶貝整理起來，一切消息都會公佈在小海 Instagram 帳號! 敬請期待 😆")
 
 st.markdown("<h2 style='text-align: center; color: black;'>謝謝今天的拜訪，你填寫的小卡之後會製作成驚喜唷！</h2>",
             unsafe_allow_html=True)
